6d7b4225f48878626bf3926463ed61f867708be9
Christian <christianajusd@gmail.com>
2015-06-24 10:14:32 -0700

    Finished final code.

--- Inform/src/com/kingsandglory/Inform.java
+++ Inform/src/com/kingsandglory/Inform.java
@@ -1,15 +1,21 @@
 package com.kingsandglory;
 
 
+//import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Inform extends JavaPlugin {
-	  public void onEnable() {
+public final class Inform extends JavaPlugin implements Listener {
+	  	@Override
+		public void onEnable() {
+		  getServer().getPluginManager().registerEvents(this, this);
 		  getLogger().info("ApplyInform has been enabled");
+		  
 	    }
 	 
 	    public void onDisable() {
@@ -17,26 +23,24 @@ public final class Inform extends JavaPlugin {
 	    }
 	
 	    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-	    	if (cmd.getName().equalsIgnoreCase("roll")) { // If the player typed /basic then do the following...
-	    		if (args.length == 1){
-	    			sender.sendMessage(Integer.toString(((int)(Math.random()*Integer.parseInt(args[0])))));
-	    		}
-	    		
-	    		
-	    		if (args.length == 0){
-	    			sender.sendMessage(Integer.toString(((int)(Math.random()*20))));
-	    		}
-	    			    		// doSomething
+	    	if (cmd.getName().equalsIgnoreCase("wut")) { // If the player typed /basic then do the following...
+	    		sender.sendMessage("yolo dude");
+	    			    		
 	    		return true;
 	    	} //If this has happened the function will return true. 
 	            // If this hasn't happened the value of false will be returned.
 	    	return false; 
 	    }
 	    
-	    public void onPlayerJoin(PlayerJoinEvent join){
+	    @EventHandler
+	    public void onJoin(PlayerJoinEvent join){
+	    	getLogger().info("Hey! some noob joined!");
 	    	Player p =  join.getPlayer();
-	    	if (p.hasPermission("Inform.recieve"))
-	    	p.sendMessage("&6&lGo to &cwww.KingAndGlory.com &6to Apply, check updates, and browse the forums!");
+	    	if (p.hasPermission("Inform.recieve")){
+	    	p.sendMessage("§6§lGo to §cwww.KingAndGlory.com §6to Apply, check updates, and browse the forums!");
+	    	}
 	    }
 }
+
+
 
